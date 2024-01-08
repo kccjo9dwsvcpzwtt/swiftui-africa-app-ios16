ef89f82adbf60e8b1263baba43b446c27b83e07b
kccjo9dwsvcpzwtt <128245629+kccjo9dwsvcpzwtt@users.noreply.github.com>
2024-01-08 13:30:56 +0100

    Initial Commit

---
+++ swiftui-africa-app-ios16/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui-africa-app-ios16
+//
+//  Created by Stanislav Ponomarenko on 2024-01-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui-africa-app-ios16/swiftui_africa_app_ios16App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_africa_app_ios16App.swift
+//  swiftui-africa-app-ios16
+//
+//  Created by Stanislav Ponomarenko on 2024-01-08.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_africa_app_ios16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
